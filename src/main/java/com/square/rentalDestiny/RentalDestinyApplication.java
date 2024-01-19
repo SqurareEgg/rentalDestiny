19522b789fb80d08acd5d136d821ae81cf7ea9ab
HKIT <junwoo256@gmail.com>
2024-01-19 11:40:55 +0900

    Initial commit

---
+++ src/main/java/com/square/rentalDestiny/RentalDestinyApplication.java
@@ -0,0 +1,13 @@
+package com.square.rentalDestiny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalDestinyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentalDestinyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/square/rentalDestiny/RentalDestinyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.square.rentalDestiny;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalDestinyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
